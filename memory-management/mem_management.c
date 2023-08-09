4db6b7f18110d6faf0469e10099e161def76dcf5
nammonman <sirapat69@outlook.com>
2023-08-09 10:19:23 +0700

    Update mem_management.c

--- memory-management/mem_management.c
+++ memory-management/mem_management.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int glob_var1 = 0;
-int glob_var2 = 0;
-int glob_var3 = 0;
+int glob_var1 = 1;
+int glob_var2 = 2;
+int glob_var3 = 3;
 
-int some_function(int input) {
-    int some_function_var = input + 1;
-    int output = input;
-    return output;
+void some_function(int input, int**b) {
+    int a = input;
+    *b = (int*)malloc(sizeof(int));
+    glob_var2 += 2;
+    printf("stack address: %p\n", &a);
+    printf("heap address: %p\n", b[input-1]);
+    printf("global var 2 address: %p\n", &glob_var2);
+    if(a < 30)
+    some_function(a+1,b);
 }
 
 int main() {
-    int a = 55;
-    int*b = (int*)malloc(a*sizeof(int));
-    
+    int*b = NULL;
+    printf("global var 1 address: %p\n", &glob_var1);
+    printf("global var 2 address: %p\n", &glob_var2);
+    printf("global var 3 address: %p\n", &glob_var3);
+    some_function(glob_var1,&b);
+    free(b);
+    return 0;
 }
