7d60f59a4cb8a6c29f1830184932eced5a91a5e9
nammonman <sirapat69@outlook.com>
2023-08-09 13:48:23 +0700

    Update mem_management.c

--- memory-management/mem_management.c
+++ memory-management/mem_management.c
@@ -5,23 +5,34 @@ int glob_var1 = 1;
 int glob_var2 = 2;
 int glob_var3 = 3;
 
-void some_function(int input, int**b) {
-    int a = input;
-    *b = (int*)malloc(sizeof(int));
-    glob_var2 += 2;
-    printf("stack address: %p\n", &a);
-    printf("heap address: %p\n", b[input-1]);
-    printf("global var 2 address: %p\n", &glob_var2);
-    if(a < 30)
-    some_function(a+1,b);
+void dangerous_function(int*b) {
+    printf("recursive malloc until stack overflow");
+    b = (int*)malloc(999*sizeof(int));
+    dangerous_function(b);
+}
+
+void some_function(int i) {
+    printf("stack depth %d\n",i);
+    if (i < 5)
+    {
+        printf("address of i variable in some_function: %p\n",&i);
+        some_function(i+1);
+    }
 }
 
 int main() {
-    int*b = NULL;
+    int*b = (int*)malloc(sizeof(int));
     printf("global var 1 address: %p\n", &glob_var1);
     printf("global var 2 address: %p\n", &glob_var2);
     printf("global var 3 address: %p\n", &glob_var3);
-    some_function(glob_var1,&b);
+    printf("local var b address: %p\n", b);
+    for (int i = 1; i < 5; i++)
+    {
+        b = (int*)malloc(sizeof(int));
+        printf("address b after malloc %d more times: %p\n", i, b);
+    }
+    
+    some_function(1);
     free(b);
     return 0;
 }
